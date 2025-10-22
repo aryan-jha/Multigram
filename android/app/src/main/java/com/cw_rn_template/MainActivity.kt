f8cc08b3298cb0c29f5b5c392156b3e412e28ba4
Aryan Jha <aryan@creativegalileo.com>
2025-10-22 11:29:00 +0530

    initial commit 2

--- android/app/src/main/java/com/cw_rn_template/MainActivity.kt
+++ android/app/src/main/java/com/cw_rn_template/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "cw_rn_template"
+  override fun getMainComponentName(): String = "Multigram"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
